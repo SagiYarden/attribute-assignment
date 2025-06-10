44cc7c31de2559bc27d64b6efd68677da09f11de
SagiYarden <yardenshagy@gmail.com>
2025-06-10 11:52:22 +0300

    remove cache manager

--- apps/microservices/weather-api/src/app/app.module.ts
+++ apps/microservices/weather-api/src/app/app.module.ts
@@ -2,16 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { WeatherService } from './weather.service';
 import { ConfigModule } from '@nestjs/config';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    CacheModule.register({
-      ttl: 3600000, // Cache for 1 hour in milliseconds
-      max: 100, // Maximum number of items in cache
-    }),
-  ],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [WeatherService],
 })
